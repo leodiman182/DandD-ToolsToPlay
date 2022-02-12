00b0f7540dc62717f1c3a13155a07e718fa4a10c
isabelanery <isabelanery242@gmail.com>
2022-02-12 19:10:03 -0300

    adiciona alterações script forms - tentando gerar opções de equipamento de acordo com clasee

--- javascript/script-forms.js
+++ javascript/script-forms.js
@@ -10,7 +10,7 @@ const fecthOptions = async (search) => {
 const inputOptions = async (opt) => {
   const options = (await fecthOptions(opt)).results;
   
-  const insideRaces = document.querySelector(`#${opt}`); 
+  const insideSelect = document.querySelector(`#${opt}`); 
 
   options.forEach(async (race) => {
     const createOption = document.createElement('option'); 
@@ -18,27 +18,90 @@ const inputOptions = async (opt) => {
     createOption.value = race.index; 
     createOption.innerText = race.name; 
 
-    insideRaces.appendChild(createOption);
+    insideSelect.appendChild(createOption);
   })
 }; 
 
-const proficienciesOptions = async () => {
-  const choseClass = document.querySelector('#classes').value;
-  console.log(choseClass); 
+// add proficiencies you get when you choose your class. \/
+const proficienciesYouGet = async () => {
+  const chosenClass = document.querySelector('#classes').value;
+  const url = `classes/${chosenClass}`
+  const getProficiencies = (await fecthOptions(url)).proficiencies;
 
+  const createWhatUGet = document.querySelector('#proficiencies-uget');
+  createWhatUGet.innerText = 'Proficiencies you get:'; 
+
+  getProficiencies.forEach((proficiency) => {
+    // console.log(proficiency);
+    const createProficiency = document.createElement('li');
+    createProficiency.innerText = proficiency.name;
+    createWhatUGet.appendChild(createProficiency);
+  })
 };
 
-const choseClass = document.querySelector('#classes'); 
-choseClass.addEventListener('change', proficienciesOptions)
+const chosenClass = document.querySelector('#classes'); 
+
+// add subclasses by class 
+const getSubclasses = async () => {
+  const chosenClass = document.querySelector('#classes').value;
+  const subclassesOptions = document.querySelector('#subclasses');
+  const url = `classes/${chosenClass}`
+  const getSubclasses = (await fecthOptions(url)).subclasses;
+  
+  getSubclasses.forEach((subclass) => {
+    const createOption = document.createElement('option'); 
+    createOption.innerText = subclass.name;
+    subclassesOptions.appendChild(createOption);
+    // console.log(subclass.name);
+  });
+};
+
+
+// add equipments by class 
+const getEquipementByClass = async () => {
+  const chosenClass = document.querySelector('#classes').value;
+  const equipmentOptions = document.querySelector('#equipment-options');
+  const url = `classes/${chosenClass}`
+  const getEquipments = (await fecthOptions(url)).starting_equipment_options;
+  
+  // console.log(getEquipments);
+
+  getEquipments.forEach((choice, index) => {
+    const createSelect = document.createElement('select'); 
+    createSelect.id = `equipment${index + 1}`; 
+    equipmentOptions.appendChild(createSelect);
+     // console.log(choice);
+    choice.from.forEach((option) => {
+      const firstKeyOpt = Object.keys(option)[0];
+      console.log(option);
+      
+      // const createOption = document.createElement('option'); 
+      // createOption.innerText = option.name;
+      // createSelect.appendChild(createOption);
+    })
+    
+    // console.log(choice.from);
+  })
+}
+
+
+
+// const getProficiencies = (await fecthOptions(url)).proficiency_choices;
+// esta birosca é um array com um obj dentro. mas n consigo acessar chamando [0], nem .key --- SOS
+
+//console.log(getProficiencies); 
 
 inputOptions('races');
 inputOptions('classes');
-inputOptions('subclasses');
+// inputOptions('subclasses');
+chosenClass.addEventListener('change', getSubclasses);
 inputOptions('alignments');
-inputOptions('equipment');
+// inputOptions('equipment');
+chosenClass.addEventListener('change', getEquipementByClass);
 // inputOptions('proficiencies');
+chosenClass.addEventListener('change', proficienciesYouGet);
 inputOptions('skills');
 inputOptions('traits');
 
 
-proficienciesOptions();
+// proficienciesYouGet();
