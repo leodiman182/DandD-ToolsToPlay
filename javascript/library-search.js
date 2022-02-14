1b69de4b400abc2b4d75558ca2f84fc4717056ca
sorinesavage <pvmg1@hotmail.com>
2022-02-14 17:49:08 -0300

    Adicionados itens dinamicamento a library

--- javascript/library-search.js
+++ javascript/library-search.js
@@ -2,7 +2,7 @@ const fetchOptions = async (search) => {
   const url = `https://www.dnd5eapi.co/api/${search}`; 
   const response = await fetch(url); 
   const data = await response.json();    
-  
+
   return data; 
 }
 
@@ -12,13 +12,29 @@ const getSelectedOption = (select) => {
   return yourChoice; 
 }
 
-const teste = async () => {
+const createSection = (data) => {
+  const value = data.results.reduce((acc, curr) => {
+    acc.push(curr.name);
+    return acc;
+  }, []);
+  
+  value.forEach((element) => {
+    let section = document.createElement('section');
+    let sectionFather = document.querySelector('.library-list-database');
+    section.className = 'section-list';
+    section.innerHTML = element;
+    sectionFather.appendChild(section);
+  });
+}
+
+const getApiAndAddPage = async (func) => {
   const url = getSelectedOption('select-search-database');
   const data = await fetchOptions(url);
   console.log(data);
+  func(data);
 }
 
 window.onload = async () => {
     let button = document.querySelector('.btn-library-search');
-    button.addEventListener('click', teste);
+    button.addEventListener('click', function(){ getApiAndAddPage(createSection) });
 }
